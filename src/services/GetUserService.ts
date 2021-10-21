98840ab55b51746eafa96b19fe6ec2597030839a
jghagemann <hagejotage@hotmail.com>
2021-10-21 20:39:56 -0300

    Hide user sensitive info

--- src/services/GetUserService.ts
+++ src/services/GetUserService.ts
@@ -1,18 +1,25 @@
 import { getRepository } from "typeorm";
 import { User } from "../models/User";
 
-
+interface IUserId {
+  id?: string;
+}
 
 class GetUserService {
-  public async execute() {
+  public async execute(data: IUserId): Promise<User | {}> {
+    const { id } = data;
     const usersRepository = getRepository(User);
-    const users = usersRepository.find();
-    if (!users) {
+    const user = await usersRepository.findOne({ id });
+    if (!user) {
       return {
-        message: "Users not found"
+        message: "Users not found",
       };
     }
-    return users;
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email
+    };
   }
 }
 
